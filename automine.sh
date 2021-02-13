3d7ca51a476eaae93520eca8db5b4636ebde1c0f
okasha <amradel55@gmail.com>
2021-02-13 08:15:40 +0200

    updates

--- automine.sh
+++ automine.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-idle=false
-idleAfter=2000     # consider idle after 3000 ms
+idle=true
+idleAfter=300000     # consider idle after 3000 ms
 
 while true; do
   idleTimeMillis=$(xprintidle)
   #echo $idleTimeMillis  # just for debug purposes.
-  if [[ $idle = false && $idleTimeMillis -gt $idleAfter && !($(ss | grep ssh))]] ; then
-    #echo "start idle"   # or whatever command(s) you want to run...
-    $(terminator  --geometry=1024x560 -e  "sudo nvidia-smi -pl 130 && sudo ~/Downloads/Compressed/ethminer_RTX30series/build/ethminer/ethminer -U -P stratum://0x859770487e79e58f65843D1bB61Dfa946A53bc67@us1.ethermine.org:4444 -v 3; echo Done - Press enter to exit; read" 
-)
+  if [[ $idle = false && $idleTimeMillis -gt $idleAfter && !($(ss | grep ssh)) && !($(pidof ethminer))]] ; then
+  
+    $(xterm -e "sudo nvidia-smi -pl 130 && nvidia-settings --assign GPUMemoryTransferRateOffsetAllPerformanceLevels=2200 && nvidia-settings --assign GPUGraphicsClockOffsetAllPerformanceLevels=-500 && sudo ~/Downloads/Compressed/ethminer_RTX30series/build/ethminer/ethminer -U -P stratum://0x859770487e79e58f65843D1bB61Dfa946A53bc67@us1.ethermine.org:4444 -v 3;")
     idle=true
   elif [[ $idle = true && !($(pidof ethminer))]] ; then
-    $(sudo nvidia-smi -pl 210)
-    #echo "end idle"     # same here.
+    $(sudo nvidia-smi -pl 210 && nvidia-settings --assign GPUMemoryTransferRateOffsetAllPerformanceLevels=0 && nvidia-settings --assign GPUGraphicsClockOffsetAllPerformanceLevels=0)
+
     idle=false
+  elif [[ $idle = false && ($(pidof ethminer))]] ; then
+    idle=true
   fi
-sleep 1      # polling interval
+sleep 60     # polling interval
 
 done
