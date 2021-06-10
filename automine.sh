6a6136fcd9d38626e1035f516aba44075411cb0b
okasha <amradel55@gmail.com>
2021-06-10 03:41:40 +0200

    update

--- automine.sh
+++ automine.sh
@@ -6,15 +6,15 @@ idleAfter=300000     # consider idle after 3000 ms
 while true; do
   idleTimeMillis=$(xprintidle)
   #echo $idleTimeMillis  # just for debug purposes.
-  if [[ $idle = false && $idleTimeMillis -gt $idleAfter && !($(ss | grep ssh)) && !($(pidof ethminer))]] ; then
+  if [[ $idle = false && $idleTimeMillis -gt $idleAfter && !($(ss | grep ssh)) && !($(pidof miner))]] ; then
   
-    $(xterm -e "sudo nvidia-smi -pl 130 && nvidia-settings --assign GPUMemoryTransferRateOffsetAllPerformanceLevels=2200 && nvidia-settings --assign GPUGraphicsClockOffsetAllPerformanceLevels=-500 && sudo ~/Downloads/Compressed/ethminer_RTX30series/build/ethminer/ethminer -U -P stratum://0x859770487e79e58f65843D1bB61Dfa946A53bc67@us1.ethermine.org:4444 -v 3;")
+    $(xterm -e "sudo nvidia-smi -pl 130 && nvidia-settings --assign GPUMemoryTransferRateOffsetAllPerformanceLevels=2200 && nvidia-settings --assign GPUGraphicsClockOffsetAllPerformanceLevels=-500 && ~/Downloads/Compressed/gminer_2_45_linux64/mine_eth.sh;")
     idle=true
-  elif [[ $idle = true && !($(pidof ethminer))]] ; then
+  elif [[ $idle = true && !($(pidof miner))]] ; then
     $(sudo nvidia-smi -pl 210 && nvidia-settings --assign GPUMemoryTransferRateOffsetAllPerformanceLevels=0 && nvidia-settings --assign GPUGraphicsClockOffsetAllPerformanceLevels=0)
 
     idle=false
-  elif [[ $idle = false && ($(pidof ethminer))]] ; then
+  elif [[ $idle = false && ($(pidof miner))]] ; then
     idle=true
   fi
 sleep 60     # polling interval
